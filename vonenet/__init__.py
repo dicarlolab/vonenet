a25b9cd49234452522ea0447c1c55ead94823895
tiagogmarques <tmarques@mit.edu>
2020-09-03 20:23:55 -0400

    Corrected a bug when loading the adversarial trained model

--- vonenet/__init__.py
+++ vonenet/__init__.py
@@ -4,12 +4,19 @@ import os
 import requests
 
 from .vonenet import VOneNet
+from torch.nn import Module
 
 FILE_WEIGHTS = {'alexnet': 'vonealexnet_e70.pth.tar', 'resnet50': 'voneresnet50_e70.pth.tar',
                 'resnet50_at': 'voneresnet50_at_e96.pth.tar', 'cornets': 'vonecornets_e70.pth.tar'}
 
 
-def get_model(model_arch=None, pretrained=True, **kwargs):
+class Wrapper(Module):
+    def __init__(self, model):
+        super(Wrapper, self).__init__()
+        self.module = model
+
+
+def get_model(model_arch=None, pretrained=True, map_location='cpu', **kwargs):
     """
     Returns a VOneNet model.
     Select pretrained=True for returning one of the 3 pretrained models.
@@ -27,7 +34,7 @@ def get_model(model_arch=None, pretrained=True, **kwargs):
             r = requests.get(url, allow_redirects=True)
             open(weightsdir_path, 'wb').write(r.content)
 
-        ckpt_data = torch.load(weightsdir_path)
+        ckpt_data = torch.load(weightsdir_path, map_location=map_location)
 
         stride = ckpt_data['flags']['stride']
         simple_channels = ckpt_data['flags']['simple_channels']
@@ -38,14 +45,21 @@ def get_model(model_arch=None, pretrained=True, **kwargs):
         noise_scale = ckpt_data['flags']['noise_scale']
         noise_level = ckpt_data['flags']['noise_level']
 
-        model = globals()[f'VOneNet'](model_arch=model_arch, stride=stride, k_exc=k_exc,
+        if model_arch.lower() == 'resnet50_at':
+            model_id = 'resnet50'
+        else:
+            model_id = model_arch
+
+        model = globals()[f'VOneNet'](model_arch=model_id, stride=stride, k_exc=k_exc,
                                       simple_channels=simple_channels, complex_channels=complex_channels,
                                       noise_mode=noise_mode, noise_scale=noise_scale, noise_level=noise_level)
 
         if model_arch.lower() == 'resnet50_at':
-            model = model.module
+            ckpt_data['state_dict'].pop('vone_block.div_u.weight')
+            ckpt_data['state_dict'].pop('vone_block.div_t.weight')
             model.load_state_dict(ckpt_data['state_dict'])
         else:
+            model = Wrapper(model)
             model.load_state_dict(ckpt_data['state_dict'])
             model = model.module
 
