2fab3cc80a6bc7a7139e145ac2a4dffa1795470d
Avinash <baidyaavinash@gmail.com>
2021-08-12 17:06:25 -0400

    Add the option for a ResNet18 backend.

--- vonenet/vonenet.py
+++ vonenet/vonenet.py
@@ -1,27 +1,33 @@
-
 from collections import OrderedDict
 from torch import nn
 from .modules import VOneBlock
-from .back_ends import ResNetBackEnd, Bottleneck, AlexNetBackEnd, CORnetSBackEnd
+from .back_ends import ResNetBackEnd, Bottleneck, AlexNetBackEnd, \
+    CORnetSBackEnd, BasicBlock
 from .params import generate_gabor_param
+
 import numpy as np
 
 
 def VOneNet(sf_corr=0.75, sf_max=9, sf_min=0, rand_param=False, gabor_seed=0,
             simple_channels=256, complex_channels=256,
             noise_mode='neuronal', noise_scale=0.35, noise_level=0.07, k_exc=25,
-            model_arch='resnet50', image_size=224, visual_degrees=8, ksize=25, stride=4):
-
-
+            model_arch='resnet50', image_size=224, visual_degrees=8, ksize=25,
+            stride=4):
     out_channels = simple_channels + complex_channels
 
-    sf, theta, phase, nx, ny = generate_gabor_param(out_channels, gabor_seed, rand_param, sf_corr, sf_max, sf_min)
+    sf, theta, phase, nx, ny = generate_gabor_param(out_channels, gabor_seed,
+                                                    rand_param, sf_corr, sf_max,
+                                                    sf_min)
 
-    gabor_params = {'simple_channels': simple_channels, 'complex_channels': complex_channels, 'rand_param': rand_param,
-                    'gabor_seed': gabor_seed, 'sf_max': sf_max, 'sf_corr': sf_corr, 'sf': sf.copy(),
-                    'theta': theta.copy(), 'phase': phase.copy(), 'nx': nx.copy(), 'ny': ny.copy()}
-    arch_params = {'k_exc': k_exc, 'arch': model_arch, 'ksize': ksize, 'stride': stride}
-
+    gabor_params = {'simple_channels': simple_channels,
+                    'complex_channels': complex_channels,
+                    'rand_param': rand_param,
+                    'gabor_seed': gabor_seed, 'sf_max': sf_max,
+                    'sf_corr': sf_corr, 'sf': sf.copy(),
+                    'theta': theta.copy(), 'phase': phase.copy(),
+                    'nx': nx.copy(), 'ny': ny.copy()}
+    arch_params = {'k_exc': k_exc, 'arch': model_arch, 'ksize': ksize,
+                   'stride': stride}
 
     # Conversions
     ppd = image_size / visual_degrees
@@ -29,21 +35,31 @@ def VOneNet(sf_corr=0.75, sf_max=9, sf_min=0, rand_param=False, gabor_seed=0,
     sf = sf / ppd
     sigx = nx / sf
     sigy = ny / sf
-    theta = theta/180 * np.pi
+    theta = theta / 180 * np.pi
     phase = phase / 180 * np.pi
 
-    vone_block = VOneBlock(sf=sf, theta=theta, sigx=sigx, sigy=sigy, phase=phase,
-                           k_exc=k_exc, noise_mode=noise_mode, noise_scale=noise_scale, noise_level=noise_level,
-                           simple_channels=simple_channels, complex_channels=complex_channels,
+    vone_block = VOneBlock(sf=sf, theta=theta, sigx=sigx, sigy=sigy,
+                           phase=phase,
+                           k_exc=k_exc, noise_mode=noise_mode,
+                           noise_scale=noise_scale, noise_level=noise_level,
+                           simple_channels=simple_channels,
+                           complex_channels=complex_channels,
                            ksize=ksize, stride=stride, input_size=image_size)
 
     if model_arch:
-        bottleneck = nn.Conv2d(out_channels, 64, kernel_size=1, stride=1, bias=False)
-        nn.init.kaiming_normal_(bottleneck.weight, mode='fan_out', nonlinearity='relu')
+        bottleneck = nn.Conv2d(out_channels, 64, kernel_size=1, stride=1,
+                               bias=False)
+        nn.init.kaiming_normal_(bottleneck.weight, mode='fan_out',
+                                nonlinearity='relu')
 
         if model_arch.lower() == 'resnet50':
             print('Model: ', 'VOneResnet50')
-            model_back_end = ResNetBackEnd(block=Bottleneck, layers=[3, 4, 6, 3])
+            model_back_end = ResNetBackEnd(block=Bottleneck,
+                                           layers=[3, 4, 6, 3])
+        elif model_arch.lower() == 'resnet18':
+            print('Model: ', 'VOneResnet18')
+            model_back_end = ResNetBackEnd(block=BasicBlock,
+                                           layers=[2, 2, 2, 2])
         elif model_arch.lower() == 'alexnet':
             print('Model: ', 'VOneAlexNet')
             model_back_end = AlexNetBackEnd()
